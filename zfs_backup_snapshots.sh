183d7e313c4501360d59486d854e5bdc58600187
Dave Snigier <dsnigier@gmail.com>
2013-10-31 13:58:48 -0400

    added cli options & root support
    dried out a few functions and added cli parameters
    added support for backing up the root of the current boot environment

--- zfs_backup_snapshots.sh
+++ zfs_backup_snapshots.sh
@@ -15,7 +15,8 @@ if [[ $? == 1 ]]; then
 	exit 1
 fi
 
-# cleanup code
+
+# functions
 # =============================================
 
 # umounts and cleans up the backup directory
@@ -32,9 +33,6 @@ function zfs_backup_cleanup() {
 	# delete empty directories from within the backup directory
 	gfind "${1}" -mindepth 1 -maxdepth 1 -type d -empty -delete
 }
-
-# creation code
-# =============================================
 
 # gets the name of the newest snapshot given a zfs filesystem
 # usage: get_latest_snap filesystem
@@ -71,16 +69,11 @@ function zfs_snapshot_mountpoint() {
 	fi
 }
 
-
-# =======
-# cleanup crap from previous runs
-zfs_backup_cleanup "${backupDirectory}"
-
-# work on non-root filesystems
-# get list of all non-root zfs filesystems on the box not including the ROOT since that has duplicate mountpoints
-filesystems=( $(zfs list -H -o name | egrep -v "^rpool/ROOT.*") )
-
-for fs in "${filesystems[@]}"; do
+# mounts latest snapshot in directory
+# usage: mount_latest_snap filesystem backupdirectory
+function mount_latest_snap() {
+	backupDirectory="${2}"
+	fs="${1}"
 
 	# get name of latest snapshot
 	snapshot=$(zfs_latest_snap "${fs}")
@@ -88,14 +81,14 @@ for fs in "${filesystems[@]}"; do
 	# if there's no snapshot then let's ignore it
 	if [[ $? == 1 ]]; then
 		echo "No snapshot exists for ${fs}, it will not be backed up."
-		continue
+		return 1
 	fi
 
 	sourcepath=$(zfs_snapshot_mountpoint "${fs}" "${snapshot}")
 	# if the filesystem is not mounted/path doesn't exist then let's ignore as well
 	if [[ $? == 1 ]]; then
 		echo "Cannot find snapshot ${snapshot} for ${fs}, perhaps it's not mounted? Anyways, it will not be backed up."
-		continue
+		return 1
 	fi
 
 	# replace filesystem slashes with underscores for use as the mount directory
@@ -104,4 +97,80 @@ for fs in "${filesystems[@]}"; do
 	# mount to backup directory using a bind filesystem
 	mkdir "${mountpath}"
 	mount -F lofs "${sourcepath}" "${mountpath}"
-done
+	return 0
+}
+
+
+function usage() {
+	echo <<-EOF
+	The following commands are supported:
+	   cleanup: Unmounts everything from the backup directory
+	     mount: Mounts the latest snapshot for every ZFS filesystem to the backup directory
+	mount-root: Mounts the latest snapshot for the root of the current boot environment
+	 mount-all: Performs cleanup, then mounts all filesystems including root to the backup directory
+	      help: You're looking at it!
+	EOF
+	exit 0
+}
+
+function cleanup() {
+	zfs_backup_cleanup "${backupDirectory}"
+	exit 0
+}
+
+
+function mount() {
+	# get list of all non-root zfs filesystems on the box not including the ROOT since that has duplicate mountpoints
+	filesystems=( $(zfs list -H -o name | egrep -v "^rpool/ROOT.*") )
+
+	for fs in "${filesystems[@]}"; do
+		mount_latest_snap "${fs}" "${backupDirectory}"
+	done
+	return 0
+}
+
+function mount-root() {
+	# get current root environment filesystem
+	rootfs="rpool/ROOT/$(beadm list | perl -nle 'print "$1" if /(solaris.*?)\s+N.*/')"
+
+	mount_latest_snap "${rootfs}" "${backupDirectory}"
+	return $?
+}
+
+function mount-all() {
+	errFlag=false;
+	# cleanup crap from previous runs
+	cleanup
+	if [[ $? != 0 ]]; then
+		errFlag=true;
+	fi
+	mount-root
+	if [[ $? != 0 ]]; then
+		errFlag=true;
+	fi
+	mount
+	if [[ $? != 0 ]]; then
+		errFlag=true;
+	fi
+	exit ${errFlag}
+}
+
+# ==========================================
+# arguments parsing
+
+if [[ $1 == "cleanup" ]]; then
+	cleanup
+elif [[ $1 == "mount" ]]; then
+	mount
+elif [[ $1 == "mount-root" ]]; then
+	mount-root
+elif [[ $1 == "mount-all" ]]; then
+	mount-all
+elif [[ $1 == "help" ]]; then
+	usage
+else
+	echo "missing command"
+	echo "For more info, run: zfs_backup_snapshots help"
+	exit 1
+fi
+
